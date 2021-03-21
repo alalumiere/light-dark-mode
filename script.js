bf7688e57a62e2edd381fd98f4b31759dd2be5d5
Amanda LaLumiere <amandalalumiere@gmail.com>
2021-03-20 23:21:20 -0500

    create toggle for dark-light mode and added local storage

--- script.js
+++ script.js
@@ -3,22 +3,54 @@ const nav = document.getElementById('nav');
 const toggleIcon = document.getElementById('toggle-icon');
 const image1 = document.getElementById('image1');
 const image2 = document.getElementById('image2');
-const image3 = document.getElementByID('image3');
-const textBox = dcument.getElementByID('text-box');
+const image3 = document.getElementById('image3');
+const textBox = document.getElementById('text-box');
+// const DARK_THEME = 'dark';
+// const LIGHT_THEME = 'light';
 
-function darkMode() {
-  nav.style.backgroundColor = 'rgb(0 0 0 / 50%';
-  textBox.style.backgroundColor = 'rgb(255 255 255 / 50%';
+function imageMode(color) {
+  image1.src = `img/undraw_proud_coder_${color}.svg`;
+  image2.src = `img/undraw_feeling_proud_${color}.svg`;
+  image3.src = `img/undraw_conceptual_idea_${color}.svg`;
+}
+
+// toggleDarkLightMode('dark');
+// toggleDarkLightMode('light');
+
+function toggleDarkLightMode(isDark) {
+  nav.style.backgroundColor = isDark
+    ? 'rgb(0 0 0 / 50%)'
+    : 'rgb(255 255 255 / 50%)';
+  textBox.style.backgroundColor = isDark
+    ? 'rgb(255 255 255 / 50%)'
+    : 'rgb(0 0 0 / 50%)';
+  toggleIcon.children[0].textContent = isDark ? 'Dark Mode' : 'Light Mode';
+  isDark
+    ? toggleIcon.children[1].classList.replace('fa-sun', 'fa-moon')
+    : toggleIcon.children[1].classList.replace('fa-moon', 'fa-sun');
+  isDark ? imageMode('dark') : imageMode('light');
 }
 
 function switchTheme(e) {
   if (e.target.checked) {
     document.documentElement.setAttribute('data-theme', 'dark');
-    darkMode();
+    localStorage.setItem('theme', 'dark');
+    toggleDarkLightMode(true);
   } else {
     document.documentElement.setAttribute('data-theme', 'light');
-    lightMode();
+    localStorage.setItem('theme', 'light');
+    toggleDarkLightMode(false);
   }
 }
 
 toggleSwitch.addEventListener('change', switchTheme);
+
+const currentTheme = localStorage.getItem('theme');
+if (currentTheme) {
+  document.documentElement.setAttribute('data-theme', currentTheme);
+
+  if (currentTheme === 'dark') {
+    toggleSwitch.checked = true;
+    toggleDarkLightMode(true);
+  }
+}
